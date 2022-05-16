00fdcf589b51c6d0f4964f236e8659e0a72d67ad
Mercurius13 <76958526+Mercurius13@users.noreply.github.com>
2022-05-16 17:20:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -17,6 +17,12 @@ slash = SlashCommand(bot, sync_commands=True)
 
 bot.remove_command('help')
 
+await bot.change_presence(
+            status=nextcord.Status.idle,
+            activity=nextcord.Activity(
+                type=nextcord.ActivityType.listening,
+                name=f"-help || Im back... kinda!"))
+
 for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
         try:
